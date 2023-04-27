35f3b87f4d06fe650f1748fe63763e6462e8a79d
VERAGUBER <VERA2@UW.EDU>
2023-04-27 10:35:52 -0700

    Started hw6

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,10 +1,12 @@
 import sqlite3  # This is the package for all sqlite3 access in Python
 import sys      # This helps with command-line parameters
 
+connection = sqlite3.connect("pokemon.sqlite")
+
 # All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+         "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+         "poison", "psychic", "rock", "steel", "water"]
 
 # Take six parameters on the command-line
 if len(sys.argv) < 6:
@@ -17,6 +19,16 @@ for i, arg in enumerate(sys.argv):
         continue
 
     # Analyze the pokemon whose pokedex_number is in "arg"
+
+for pokemon in sys.argv:
+    print("Analyzing" + str(pokemon))
+    type_ids = connection.execute(
+        "SELECT type_id FROM pokemon_type WHERE pokemon_id=", pokemon)
+    for id in type_ids:
+        current_type = connection.execute(
+            "SELECT name FROM type WHERE id=", id)
+    print("Bulbasaur (" + str(current_type) +
+          ") is strong against [] but weak against []")
 
     # You will need to write the SQL, extract the results, and compare
     # Remember to look at those "against_NNN" column values; greater than 1
@@ -31,4 +43,3 @@ if answer.upper() == "Y" or answer.upper() == "YES":
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
